ecb4effe62d9579233c32d80e4180d1facad6b1e
Amirreza <amirreza.moosavi98@gmail.com>
2023-10-02 14:54:57 +0330

    codes for notations, values and environments in part 2

--- Part_two.R
+++ Part_two.R
@@ -51,3 +51,30 @@ my_list <- list(100:130, "R", list(TRUE, FALSE))
 # Data Frame: two-dimensional version of list
 # Saving data:
 write.csv(deck, file = "cards.csv", row.names = FALSE)
+
+
+
+# R notations
+# Selecting values: Positive integers, Negative integers, Zero, Blank spaces,
+# Logical Values, Names
+# Positive integers: like "ij" notation in linear algebra,
+# ith row and jth column
+deck[1, c(1,2,3)]
+as_vector <- deck[1:3, 1]
+as_dataframe <- deck[1:3, 1:2]
+as_dataframe2 <- deck[1:3, 1, drop = FALSE]
+
+# Negative integers: every element except elements in negative index
+as_dataframe3 <- deck[-(4:52), 1:3]
+
+# Zero: returns nothing
+as_zero <- deck[0, 1:3]
+
+# Blank spaces: extracts every value in a dimension
+as_dataframe4 <- deck[1:5, ]
+
+# Logical values: matches each true and false to a column or row
+as_dataframe5 <- deck[1:5, c(FALSE, TRUE, TRUE)]
+
+# Names: 
+as_dataframe6 <- deck[1:2, c("face", "suit")]
